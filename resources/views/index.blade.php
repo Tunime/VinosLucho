9d8e9ffd6fd5f29367e94c0c3b8b3fb571d23cec
tunime <tunime.1@gmail.com>
2018-07-19 09:52:34 -0500

    first

---
+++ resources/views/index.blade.php
@@ -0,0 +1,164 @@
+<!--A Design by W3layouts 
+Author: W3layout
+Author URL: http://w3layouts.com
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/
+-->
+<!DOCTYPE html>
+<html>
+<head>
+<title>Montoya</title>
+<link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="js/jquery.min.js"></script>
+<!-- Custom Theme files -->
+<!--theme-style-->
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />	
+<!--//theme-style-->
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="Mattress Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, 
+Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyErricsson, Motorola web design" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+<!--fonts-->
+<link href='//fonts.googleapis.com/css?family=Lato:100,300,400,700,900' rel='stylesheet' type='text/css'>
+<link href='//fonts.googleapis.com/css?family=Roboto:400,100,300,500,700,900' rel='stylesheet' type='text/css'><!--//fonts-->
+<!-- start menu -->
+<link href="css/memenu.css" rel="stylesheet" type="text/css" media="all" />
+<script type="text/javascript" src="js/memenu.js"></script>
+<script>$(document).ready(function(){$(".memenu").memenu();});</script>
+<script src="js/simpleCart.min.js"> </script>
+</head>
+<body>
+<!--header-->
+<div class="header">
+	
+		<div class="container">
+			<div class="head-top">
+				<div class="logo">
+					<h1><a href="index.html">Montoya</a></h1>
+				</div>
+		  <div class=" h_menu4">
+				<ul class="memenu skyblue">
+				<li><a class="color4" href="/login">Login</a></li>				
+			  </ul> 
+			</div>
+				
+				<div class="clearfix"> </div>
+		</div>
+		</div>
+	</div>
+	<div class="banner">
+		<div class="container">
+			  <script src="js/responsiveslides.min.js"></script>
+  <script>
+    $(function () {
+      $("#slider").responsiveSlides({
+      	auto: true,
+      	nav: true,
+      	speed: 500,
+        namespace: "callbacks",
+        pager: true,
+      });
+    });
+  </script>
+			<div  id="top" class="callbacks_container">
+			<ul class="rslides" id="slider">
+			    <li>
+					
+						<div class="banner-text">
+							<h3>Vinos Montoya</h3>
+						<p>Los mejores vinos con la mejor calidad a qui en vinos montoya</p>
+						
+						</div>
+				
+				</li>
+				<li>
+					
+						<div class="banner-text">
+							<h3>Vino montoya </h3>
+						<p>Los mejores vinos con la mejor calidad a qui en vinos montoya</p>
+												
+
+						</div>
+					
+				</li>
+				<li>
+						<div class="banner-text">
+							<h3>Vinos montoya</h3>
+						<p>Los mejores vinos con la mejor calidad a qui en vinos montoya</p>
+								
+
+						</div>
+					
+				</li>
+			</ul>
+		</div>
+
+	</div>
+	</div>
+
+<!--content-->
+<div class="container">
+	<div class="cont">
+		<div class="content">
+			<div class="content-top-bottom">
+				<h2>Nuestros productos</h2>
+				<div class="col-md-6 men">
+					<a href="single.html" class="b-link-stripe b-animate-go  thickbox"><img class="img-responsive" src="images/t1.jpg" alt="">
+						<div class="b-wrapper">
+											<h3 class="b-animate b-from-top top-in   b-delay03 ">
+												<span>TRIBECA LIVING</span>	
+											</h3>
+										</div>
+					</a>
+					
+					
+				</div>
+				<div class="col-md-6">
+					<div class="col-md1 ">
+						<a href="single.html" class="b-link-stripe b-animate-go  thickbox"><img class="img-responsive" src="images/t2.jpg" alt="">
+							<div class="b-wrapper">
+											<h3 class="b-animate b-from-top top-in1   b-delay03 ">
+												<span>CLARISSA</span>	
+											</h3>
+										</div>
+						</a>
+						
+					</div>
+					<div class="col-md2">
+						<div class="col-md-6 men1">
+							<a href="single.html" class="b-link-stripe b-animate-go  thickbox"><img class="img-responsive" src="images/t3.jpg" alt="">
+									<div class="b-wrapper">
+											<h3 class="b-animate b-from-top top-in2   b-delay03 ">
+												<span>COLORMATE</span>	
+											</h3>
+										</div>
+							</a>
+							
+						</div>
+						<div class="col-md-6 men2">
+							<a href="single.html" class="b-link-stripe b-animate-go  thickbox"><img class="img-responsive" src="images/t4.jpg" alt="">
+									<div class="b-wrapper">
+											<h3 class="b-animate b-from-top top-in2   b-delay03 ">
+												<span>HERLEQUIN</span>	
+											</h3>
+										</div>
+							</a>
+							
+						</div>
+						<div class="clearfix"> </div>
+					</div>
+				</div>
+				<div class="clearfix"> </div>
+			</div>
+			
+			</div>
+		</div>
+	<!----->
+	</div>
+	<!---->
+</div>
+</body>
+</html>
+			
---
+++ resources/views/login.blade.php
@@ -0,0 +1,94 @@
+<!--A Design by W3layouts 
+Author: W3layout
+Author URL: http://w3layouts.com
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/
+-->
+<!DOCTYPE html>
+<html>
+<head>
+<title>Montoya</title>
+<link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="js/jquery.min.js"></script>
+<!-- Custom Theme files -->
+<!--theme-style-->
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />	
+<!--//theme-style-->
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="Mattress Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, 
+Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyErricsson, Motorola web design" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+<!--fonts-->
+<link href='//fonts.googleapis.com/css?family=Lato:100,300,400,700,900' rel='stylesheet' type='text/css'>
+<link href='//fonts.googleapis.com/css?family=Roboto:400,100,300,500,700,900' rel='stylesheet' type='text/css'><!--//fonts-->
+<!-- start menu -->
+<link href="css/memenu.css" rel="stylesheet" type="text/css" media="all" />
+<script type="text/javascript" src="js/memenu.js"></script>
+<script>$(document).ready(function(){$(".memenu").memenu();});</script>
+<script src="js/simpleCart.min.js"> </script>
+</head>
+<body>
+<!--header-->
+<div class="header">
+	
+		</div>
+		<div class="container">
+			<div class="head-top">
+				<div class="logo">
+					<h1><a href="/">Montoya</a></h1>
+				</div>
+		  <div class=" h_menu4">
+				<ul class="memenu skyblue">
+				<li><a class="color4" href="/login">Login</a></li>				
+			  </ul> 
+			</div>
+				
+				<div class="clearfix"> </div>
+		</div>
+		</div>
+	</div>
+	<!-- grow -->
+	<div class="grow">
+		<div class="container">
+			<h2>Login</h2>
+		</div>
+	</div>
+	<!-- grow -->
+<!--content-->
+<div class="container">
+		<div class="account">
+		<div class="account-pass">
+		<div class="col-md-8 account-top">
+			<form action="#" method="post">
+				
+			<div> 	
+				<span>Email</span>
+				<input type="text"> 
+			</div>
+			<div> 
+				<span >Password</span>
+				<input type="password">
+			</div>				
+				<input type="submit" value="Login"> 
+			</form>
+		</div>
+		<div class="col-md-4 left-account ">
+			<a href="single.html"><img class="img-responsive " src="images/s1.jpg" alt=""></a>
+			<div class="five">
+			<h2>25% </h2><span>discount</span>
+			</div>
+			<a href="register.html" class="create">Create an account</a>
+<div class="clearfix"> </div>
+		</div>
+	<div class="clearfix"> </div>
+	</div>
+	</div>
+
+</div>
+
+
+</body>
+</html>
+			
